09fdcb368723cab83bec80e06dbd07425b7e4c38
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-02-27 02:43:11 -0600

    intialized global types

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+export type UserData = {
+  displayName: string | null;
+  email: string;
+  emailVisible: boolean;
+  newsletter: boolean;
+  photoURL: string | null;
+  portfolioURL: string | null;
+  setup: boolean;
+  title: string | null;
+  uid: string;
+};
