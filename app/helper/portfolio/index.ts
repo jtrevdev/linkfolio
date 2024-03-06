7b85fdbf17c9421b1e353b3d953f1c56f00f6a11
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-03-05 22:45:59 -0600

    adjusting delay for puppeteer screenshot

--- app/helper/portfolio/index.ts
+++ app/helper/portfolio/index.ts
@@ -1,7 +1,12 @@
 'use server';
 
+import { firestore, storage } from '@/app/firebase/config';
+import { update } from 'firebase/database';
+import { addDoc, collection, doc, setDoc } from 'firebase/firestore';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 import puppeteer from 'puppeteer';
 
+// Handle User Changing Linked Portfolio Url, Send Back An Image Of New One
 export async function portfolioImageUpload(url: string, user_id: string) {
   let browser = null;
 
@@ -14,7 +19,7 @@ export async function portfolioImageUpload(url: string, user_id: string) {
     await page.setViewport({ width: 1280, height: 709 });
 
     // Set timer for users web page contents to fully load
-    // await new Promise((resolve) => setTimeout(() => resolve(''), 3000));
+    await new Promise((resolve) => setTimeout(() => resolve(''), 3500));
 
     // Take a screenshot
     const screenshot = await page.screenshot();
@@ -25,5 +30,19 @@ export async function portfolioImageUpload(url: string, user_id: string) {
     return serializedData;
   } catch (error) {
     console.log(error);
+  }
+}
+
+// Handle User Changing Linked Portfolio Url, Update Portfolio DB With New Data
+export async function portfolioUpload(
+  portfolioUrl: string,
+  portfolioImgURL: any,
+  user_uid: string
+) {
+  try {
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
   }
 }
