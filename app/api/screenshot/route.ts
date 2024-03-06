2b91c43ad9af240500e3c7efc0c7658514ab1997
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-03-05 22:45:31 -0600

    deleting unused route

--- app/api/screenshot/route.ts
+++ app/api/screenshot/route.ts
@@ -1,59 +1,59 @@
-import puppeteer from 'puppeteer';
-import { NextRequest, NextResponse } from 'next/server';
-import { getDownloadURL, ref, uploadString } from 'firebase/storage';
+// import puppeteer from 'puppeteer';
+// import { NextRequest, NextResponse } from 'next/server';
+// import { getDownloadURL, ref, uploadString } from 'firebase/storage';
 
-import { storage } from '@/app/firebase/config';
+// import { storage } from '@/app/firebase/config';
 
-export async function POST(req: NextRequest) {
-  const { url, user_id, auth } = await req.json();
+// export async function POST(req: NextRequest) {
+//   const { url, user_id, auth } = await req.json();
 
-  // Condition checked prior to submitting
-  // if (!url) {
-  //   return NextResponse.json({ message: 'No url provided' }, { status: 400 });
-  // }
+//   // Condition checked prior to submitting
+//   // if (!url) {
+//   //   return NextResponse.json({ message: 'No url provided' }, { status: 400 });
+//   // }
 
-  let browser = null;
+//   let browser = null;
 
-  try {
-    browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
+//   try {
+//     browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto(url);
 
-    // Adjust viewport and wait for any lazy-loaded content
-    await page.setViewport({ width: 1280, height: 709 });
+//     // Adjust viewport and wait for any lazy-loaded content
+//     await page.setViewport({ width: 1280, height: 709 });
 
-    // Set timer for users web page contents to fully load
-    // await new Promise((resolve) => setTimeout(() => resolve(''), 3000));
+//     // Set timer for users web page contents to fully load
+//     // await new Promise((resolve) => setTimeout(() => resolve(''), 3000));
 
-    // Take a screenshot
-    const screenshot = await page.screenshot();
+//     // Take a screenshot
+//     const screenshot = await page.screenshot();
 
-    // Close the browser
-    await browser.close();
+//     // Close the browser
+//     await browser.close();
 
-    // If screenshot successful, grab download url
-    console.log(Date.now());
-    if (screenshot) {
-      const storageRef = ref(storage, '/profile/' + user_id + '.png');
+//     // If screenshot successful, grab download url
+//     console.log(Date.now());
+//     if (screenshot) {
+//       const storageRef = ref(storage, '/profile/' + user_id + '.png');
 
-      const downloadURL = await uploadString(
-        storageRef,
-        screenshot.toString('base64'),
-        'base64',
-        {
-          contentType: 'image/png',
-        }
-      ).then(() => {
-        return getDownloadURL(storageRef);
-      });
+//       const downloadURL = await uploadString(
+//         storageRef,
+//         screenshot.toString('base64'),
+//         'base64',
+//         {
+//           contentType: 'image/png',
+//         }
+//       ).then(() => {
+//         return getDownloadURL(storageRef);
+//       });
 
-      return NextResponse.json({ downloadURL }, { status: 200 });
-    }
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json(
-      { message: `Error screenshotting website, ${error}` },
-      { status: 400 }
-    );
-  }
-}
+//       return NextResponse.json({ downloadURL }, { status: 200 });
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     return NextResponse.json(
+//       { message: `Error screenshotting website, ${error}` },
+//       { status: 400 }
+//     );
+//   }
+// }
