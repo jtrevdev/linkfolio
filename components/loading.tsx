94434d27dbab25b19d6f5a7726846c3b4c894012
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-03-20 03:47:44 -0500

    initializing loader component for fetching portfolios

---
+++ components/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <section>
+      <div className='w-full rounded-[8px] border-b border-border bg-border object-cover p-[17px]'></div>
+    </section>
+  );
+};
+
+export default Loading;
