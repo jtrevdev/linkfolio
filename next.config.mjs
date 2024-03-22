a3445c6fe2941c5c38ef97085d2f0c78d1ac0818
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-03-22 15:39:34 -0500

    removing deprecated image-domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com', 'firebasestorage.googleapis.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.google.usercontent.com',
+        port: '',
+        pathname: '/*',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '**',
+      },
+      // 'lh3.googleusercontent.com',
+      // 'firebasestorage.googleapis.com',
+    ],
   },
 };
 
