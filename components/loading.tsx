5622414d0740bc9531619a381eeb6cf2227a6175
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-07-03 00:46:12 -0500

    simplifying loading

--- components/loading.tsx
+++ components/loading.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 
 const Loading = () => {
   return (
-    <section className='flex flex-col justify-between rounded-[20px] border border-border bg-white'>
-      <div className='skeleton aspect-video w-full rounded-[8px] border-b border-border bg-border object-cover p-[17px]'></div>
-      <div className='flex gap-[10px] px-[17px] py-[23px]'>
-        <div className='skeleton h-[68px] w-[68px] rounded-full bg-border'></div>
-        <div className='flex w-full flex-col items-start gap-[10px]'>
-          <span className='skeleton h-[25px] w-1/2 bg-border'></span>
-          <span className='skeleton h-[25px] w-1/3 bg-border'></span>
+    <section className='translate-y-[10px]'>
+      <div className='skeleton aspect-video w-full border-b border-border bg-border object-cover '></div>
+      <div className='mt-[8px] flex w-full items-center justify-between'>
+        <div className='flex w-full items-center gap-[10px]'>
+          <div className='relative h-[35px] w-[35px]'>
+            <div className='skeleton h-full  rounded-full bg-border'></div>
+          </div>
+
+          <span className='skeleton h-[15px] w-1/2 bg-border'></span>
+        </div>
+        <div className='flex w-full justify-between gap-[10px]'>
+          <div className='flex w-full justify-end gap-5'>
+            <span className='skeleton h-[15px] w-1/3 bg-border'></span>
+
+            <span className='skeleton h-[15px] w-1/3 bg-border'></span>
+          </div>
         </div>
       </div>
     </section>
