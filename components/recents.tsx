ccb91b5b1743aa92cdd0dcbc07707a18b2132226
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-07-03 00:47:53 -0500

    removing logic from component

--- components/recents.tsx
+++ components/recents.tsx
@@ -1,49 +1,32 @@
 'use client';
-import { firestore } from '@/app/firebase/config';
 import { PortfolioData } from '@/types';
-import { collection, getDocs, limit, orderBy, query } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import Preview from './preview';
 import Loading from './loading';
+import { paginatePortfolios } from '@/lib/portfolio';
 
 const Recents = () => {
+  let n = 6;
   const [recentPortfolios, setRecentPortfolios] = useState<
     PortfolioData[] | null
   >(null);
-  const n = 6; // Amount of loaders to intialize
   useEffect(() => {
-    if (!recentPortfolios) {
-      handleRecentPortfolios();
-    }
+    getPortfolios();
   }, []);
-  async function handleRecentPortfolios() {
-    const q = query(
-      collection(firestore, 'portfolios'),
-      orderBy('timestamp', 'desc'),
-      limit(6)
-    );
-
-    const portfoliosSnapshot = await getDocs(q);
-    const combinedPosts = portfoliosSnapshot.docs.map((doc) => ({
-      photoURL: doc.data().photoURL,
-      portfolioURL: doc.data().portfolioURL,
-      views: doc.data().views,
-      owner_displayName: doc.data().owner_displayName,
-      owner_photoURL: doc.data().owner_photoURL,
-      owner_title: doc.data().owner_title,
-      timestamp: doc.data().timestamp,
-      uid: doc.id,
-    }));
-
-    setRecentPortfolios(combinedPosts);
+  async function getPortfolios() {
+    const response = await paginatePortfolios(null);
+    if (response) {
+      setRecentPortfolios(response.portfolios);
+    }
   }
+  console.log(recentPortfolios);
   return (
-    <div className='grid grid-cols-1 gap-[20px] second:grid-cols-2 first:grid-cols-3'>
+    <div className='grid grid-cols-1 gap-[20px] md:grid-cols-2 xl:grid-cols-3'>
       {recentPortfolios
         ? recentPortfolios.map((portfolio, index) => (
             <Preview
               key={index}
-              redirect={portfolio.uid}
+              redirect={portfolio.id}
               image={portfolio.photoURL}
               owner_displayName={portfolio.owner_displayName}
               owner_photoURL={portfolio.owner_photoURL}
