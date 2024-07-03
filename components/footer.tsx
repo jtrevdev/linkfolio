795bd7abe700c534a59ed2f3114cd4782eb30332
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-07-03 00:46:05 -0500

    simplifying footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,56 +1,21 @@
-import Link from 'next/link';
 import React from 'react';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <footer className=''>
-      <div className='mb-[45px] grid gap-[40px] second:grid-cols-2 second:gap-[0px]'>
-        <section className='col-span-1'>
-          <span className='flex flex-col gap-[11px]'>
-            <h1 className='text-[14px] font-semibold text-important'>About</h1>
-            <p className='text-[14px] text-general'>
-              Straight-forward real-world portfolios for your inspiration.
-            </p>
-            {/* <section className='mt-[20px] flex flex-row gap-[8px]'> */}
-            {/*   <button className='h-[50px] w-[50px] rounded-full bg-[color:#717171]'></button> */}
-            {/*   <button className='h-[50px] w-[50px] rounded-full bg-[color:#717171]'></button> */}
-            {/*   <button className='h-[50px] w-[50px] rounded-full bg-[color:#717171]'></button> */}
-            {/* </section> */}
-          </span>
-        </section>
-        <section className='col-span-1 grid grid-cols-3'>
-          <span className='flex flex-col space-y-[11px]'>
-            <h1 className='text-[14px] font-semibold text-important'>
-              Useful Links
-            </h1>
-            <Link className='w-fit text-[14px] text-general' href='/'>
-              Home
-            </Link>
-            <Link className='w-fit text-[14px] text-general' href='/portfolios'>
-              Portfolios
-            </Link>
-            <Link className='w-fit text-[14px] text-general' href='/contact'>
-              Contact Us
-            </Link>
-          </span>
-          <span className='flex flex-col space-y-[11px]'>
-            <h1 className='text-[14px] font-semibold text-important'>
-              Contact
-            </h1>
-            <button className='flex w-fit flex-row gap-[8px] text-[14px] text-general'>
-              yeahimjt@gmail.com
-            </button>
-          </span>
-        </section>
+    <footer className='my-[17px] flex h-[60px] items-center justify-between rounded-[20px] bg-[color:#F5F6F7] px-[40px]'>
+      <div className='flex items-center gap-3'>
+        Built by{' '}
+        <a href='https://yeahimjt.me' target='_blank'>
+          <Image
+            className='rounded-full'
+            src='/images/jonathan-trevino.png'
+            width={35}
+            height={35}
+            alt='Jonathan Trevino'
+          />
+        </a>
       </div>
-      {/* <div> */}
-      {/*   <section className='mb-[81px] border-t border-border'> */}
-      {/*     <p className='mt-[18px] text-center text-[14px] text-unimportant'> */}
-      {/*       All Rights Reserved @ 2024. Designed and Developed by Jonathan */}
-      {/*       Trevino */}
-      {/*     </p> */}
-      {/*   </section> */}
-      {/* </div> */}
     </footer>
   );
 };
